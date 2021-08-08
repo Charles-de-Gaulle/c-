cf1d780488e918ec71bd2d79c733b0bf7bca2b60
Charles-de-Gaulle <88604224+Charles-de-Gaulle@users.noreply.github.com>
2021-08-08 11:25:14 +0800

    Add files via upload

---
+++ test1.cpp
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	int sum = 0;
+//	scanf("%d%d", &num1, &num2);
+//	sum = num1 + num2;
+//	printf("sum=%d", sum);
+//	return 0;
+//}
+//int main()
+//{
+//	char arr1[] = "ab\0c";
+//	char arr2[] = { 'a','b','c' ,'\0'};
+//	printf("%d\n", strlen(arr1));
+//	printf("%d\n", strlen(arr2));
+//	return 0;
+//}
+//int main()
+//{
+//	printf("%d",strlen("C:\test\32\test.c"));
+//	return 0;
+//}
+//int main()
+//{
+//	int line = 0;
+//	while (line < 20000)
+//	{
+//		printf("ÇÃ´úÂë\n");
+//		line++;
+//	}
+//	printf("good offer\n");
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[10] = {  };
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		printf("%c\n", arr[i]);
+//		i++;
+//	};
+//	return 0;
+//}
