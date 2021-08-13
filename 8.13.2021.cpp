4e2a9060e8ff6224ddea9c2afd583d8ba08f3a6f
Charles-de-Gaulle <88604224+Charles-de-Gaulle@users.noreply.github.com>
2021-08-13 19:50:39 +0800

    8.13.2021

---
+++ 8.13.2021.cpp
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int b = 0;
+//		while (i < 100)
+//		{
+//			i = i++;
+//			b = i % 2;
+//			if (1 == b)
+//				printf("%d\n", i);
+//
+//		}
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 2;
+//	int b = 2;
+//	if (i < 100)
+//	{
+//		
+//		if (b < i)
+//			b = b++;
+//		if (i % b == 0)
+//			printf("%d ", i);
+//		else
+//			i = i++;
+//	}
+//
+//	return 0;
+//}
