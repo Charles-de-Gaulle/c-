d65c8dec090645e9598917f3ce8a61a58a9b6202
Charles-de-Gaulle <88604224+Charles-de-Gaulle@users.noreply.github.com>
2021-09-02 19:22:53 +0800

    9.2.2021

--- 8.27.2021.cpp
+++ 8.27.2021.cpp
@@ -8,13 +8,13 @@
 //	//printf("%d", strlen(arr));
 //	return 0;
 //}
-int main()
-{
-	char arr1[] = "aasas";
-	char arr3[] = { 'a','a','s','a','s' };
-	printf("%d\n", strlen(arr1));
-	printf("%d\n", strlen(arr3));
-	printf("%d\n", sizeof(arr1));
-	printf("%d\n", sizeof(arr3));
-	return 0;
-}
+//int main()
+//{
+//	char arr1[] = "aasas";
+//	char arr3[] = { 'a','a','s','a','s' };
+//	printf("%d\n", strlen(arr1));
+//	printf("%d\n", strlen(arr3));
+//	printf("%d\n", sizeof(arr1));
+//	printf("%d\n", sizeof(arr3));
+//	return 0;
+//}
